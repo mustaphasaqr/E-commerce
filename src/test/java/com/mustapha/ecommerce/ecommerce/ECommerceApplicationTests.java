da25e84caaebc28f415f1a30a0908720b9eb2bf7
User <user@example.com>
2025-12-19 18:07:38 +0200

    commit

--- src/test/java/com/mustapha/ecommerce/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/mustapha/ecommerce/ecommerce/ECommerceApplicationTests.java
@@ -2,9 +2,7 @@ package com.mustapha.ecommerce.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ECommerceApplicationTests {
 
--- src/test/java/com/mustapha/ecommerce/ecommerce/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.mustapha.ecommerce.ecommerce;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
